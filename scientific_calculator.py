72c2c2172aa9938bfc1c4ae1ff5b8f78e7f885ff
pris0213 <priscila.vivian@acad.pucrs.br>
2019-07-09 20:07:26 -0300

    Adding functions to Scientific Calculator

--- scientific_calculator.py
+++ scientific_calculator.py
@@ -5,3 +5,11 @@ class ScientificCalculator(Calculator):
 
     def power(self, first_number, second_number):
         return math.pow(first_number, second_number)
+
+    def calc_circle_radius(self, radius):
+        return math.pi * radius ** 2
+
+    def get_pi(self):
+        return math.pi
+
+    
